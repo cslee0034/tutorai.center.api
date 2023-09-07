866c991e39c60af38dd56c2c26f7f5040b57b32d
cslee0034 <cslee0034@gmail.com>
2023-09-07 22:41:29 +0900

    fix: fix exception provider nest-provider

--- src/common/filters/all-exception.filter.ts
+++ src/common/filters/all-exception.filter.ts
@@ -7,10 +7,13 @@ import {
   Logger,
   Inject,
 } from '@nestjs/common';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
-  constructor(@Inject('winston') private readonly logger: Logger) {}
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
 
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
