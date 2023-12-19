1eb122ca7ed55f8c6178f770a96b5ef8369151c1
cslee0034 <cslee0034@gmail.com>
2023-12-20 03:36:25 +0900

    feat: add RDB config

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,4 +8,13 @@ export const env = () => ({
     timeout: process.env.HTTP_TIMEOUT,
     max_redirects: process.env.HTTP_MAX_REDIRECTS,
   },
+  rdb: {
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+    synchronize: process.env.DB_SYNCHRONIZE,
+    logging: process.env.DB_LOGGING,
+  },
 });
--- src/config/env.validator.ts
+++ src/config/env.validator.ts
@@ -6,4 +6,11 @@ export const validationSchema = Joi.object({
   CENTER_API_PORT: Joi.number().required(),
   HTTP_TIMEOUT: Joi.number().required(),
   HTTP_MAX_REDIRECTS: Joi.number().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
+  DB_SYNCHRONIZE: Joi.boolean().required(),
+  DB_LOGGING: Joi.boolean().required(),
 });
