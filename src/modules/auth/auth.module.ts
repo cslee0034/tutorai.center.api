7d600de701d79990bcc9d9def8f2df3a76f0210c
cslee0034 <cslee0034@gmail.com>
2023-12-25 19:26:13 +0900

    feat: add auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { JWTModule } from 'src/infrastructure/token/jwt.module';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [JWTModule],
-  providers: [],
-  exports: [],
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
