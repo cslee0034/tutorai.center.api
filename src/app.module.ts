8997bfc13ec030b513611eb7f6e28db1434fc1b4
cslee0034 <cslee0034@gmail.com>
2023-12-25 15:07:20 +0900

    refactor: import JWT module and Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { GlobalConfigModule as ConfigModule } from './infrastructure/config/config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { env } from './config/env';
-import { validationSchema } from './config/env.validator';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { HttpRequestModule as HttpModule } from './infrastructure/http/http.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { RdbTypeOrmModule as TypeOrmModule } from './infrastructure/orm/rdb.typeorm.module';
+import { JWTModule } from './infrastructure/token/jwt.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      cache: true, // 설정에 대해서 캐싱
-      isGlobal: true, // 전역 모듈화
-      load: [env], // env에서 설정 값을 가져온다
-      validationSchema, // 유효성 검증 스키마
-      validationOptions: {
-        abortEarly: true, // 유효성 검사 과정 중 처음 오류 발생 즉시 중지
-      },
-    }),
-    LoggerModule,
+    ConfigModule,
     HttpModule,
+    LoggerModule,
     TypeOrmModule,
+    JWTModule,
     AuthModule,
     UserModule,
   ],
