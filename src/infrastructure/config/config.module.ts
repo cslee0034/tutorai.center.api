2beb2b466306907d85cf12d8bffa698799b3381a
cslee0034 <cslee0034@gmail.com>
2023-12-25 15:06:57 +0900

    refactor: seperate config.module into infrastructure

---
+++ src/infrastructure/config/config.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { env } from '../../config/env';
+import { validationSchema } from 'src/config/env.validator';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      cache: true, // 설정에 대해서 캐싱
+      isGlobal: true, // 전역 모듈화
+      load: [env], // env에서 설정 값을 가져온다
+      validationSchema, // 유효성 검증 스키마
+      validationOptions: {
+        abortEarly: true, // 유효성 검사 과정 중 처음 오류 발생 즉시 중지
+      },
+    }),
+  ],
+})
+export class GlobalConfigModule {}
