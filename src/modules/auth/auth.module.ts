7e914cca892162450c732b0dbb8c033b030d13ae
cslee0034 <cslee0034@gmail.com>
2023-12-25 15:06:28 +0900

    refactor: seperate JWT module into infrastructure

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,18 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { getJwtConfig } from 'src/config/jwt';
+import { JWTModule } from 'src/infrastructure/token/jwt.module';
 
 @Module({
-  imports: [
-    PassportModule.register({ defaultStrategy: 'jwt', session: true }),
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => getJwtConfig(configService),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [JWTModule],
   providers: [],
   exports: [],
 })
