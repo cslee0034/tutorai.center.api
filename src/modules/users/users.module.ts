dbc16df5b68f61061b2fb6bf55f12888195a054c
cslee0034 <cslee0034@gmail.com>
2024-01-08 23:10:41 +0900

    fix: fix entity path

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from './user.entity';
+import { UserEntity } from './entitiy/user.entity';
 import { UsersRepository } from './users.repository';
 import { AuthModule } from '../auth/auth.module';
 
