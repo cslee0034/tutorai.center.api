bb2c4b132a487a55c14fed00985e442524923a35
cslee0034 <cslee0034@gmail.com>
2024-02-04 21:58:25 +0900

    fix: fix controller의 test name UsersController로 변경

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 
-describe('AuthController', () => {
+describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
