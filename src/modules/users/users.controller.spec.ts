ee2fe9fefc5a3039ec59fb89c9f41c6920ae8e03
cslee0034 <cslee0034@gmail.com>
2024-03-01 16:41:49 +0900

    test: user.controller의 create user 테스트 추가

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -2,11 +2,24 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UserRepository } from './users.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserEntity } from './entities/user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
-  const mockUserService = {};
+  const mockUserService = {
+    create: jest.fn(async (createUserDto: CreateUserDto) => {
+      const email = createUserDto.email;
+      const name = createUserDto.name;
+      const createdAt = new Date();
+      const updatedAt = new Date();
+
+      return Promise.resolve(
+        new UserEntity({ email, name, createdAt, updatedAt }),
+      );
+    }),
+  };
 
   const mockUserRepository = {};
 
@@ -30,5 +43,29 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create user', () => {
+    const mockCreateUserDto: CreateUserDto = {
+      email: 'test@email.com',
+      name: 'test_name',
+      password: 'test_password',
+    };
+
+    it('should be defined', () => {
+      expect(controller.create).toBeDefined();
+    });
+
+    it('should called with CreateUserDto', async () => {
+      await controller.create(mockCreateUserDto as CreateUserDto);
+
+      expect(mockUserService.create).toBeCalledWith(mockCreateUserDto);
+    });
+
+    it('should return instance of UserEntity', async () => {
+      const newUser = await controller.create(mockCreateUserDto);
+
+      expect(newUser).toBeInstanceOf(UserEntity);
+    });
   });
 });
