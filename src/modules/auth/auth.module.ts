0beddb3753b137927dbd01099d3e4617cbaf109b
cslee0034 <cslee0034@gmail.com>
2024-03-03 23:47:43 +0900

    feat: EncryptModule import

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,9 +4,14 @@ import { AuthController } from './auth.contoller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './strategies/local.strategy';
+import { EncryptModule } from '../encrypt/encrypt.module';
 
 @Module({
-  imports: [JwtModule.register({}), forwardRef(() => UsersModule)],
+  imports: [
+    JwtModule.register({}),
+    EncryptModule,
+    forwardRef(() => UsersModule),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
