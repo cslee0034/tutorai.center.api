3465e664833ed7cc1bbff1f2a792f3ba261964a3
cslee0034 <cslee0034@gmail.com>
2024-03-03 12:13:51 +0900

    feat: signup에서 create user를 호출 테스트 추가

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '../users/users.service';
+import { UserEntity } from '../users/entities/user.entity';
+import { SignUpDto } from './dto/signup.dto';
 
 describe('AuthService', () => {
   let service: AuthService;
 
+  const mockUsersService = {
+    create: jest.fn(({ email, name, password }: SignUpDto) => {
+      return new UserEntity({ email, name, password });
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
@@ -17,8 +32,20 @@ describe('AuthService', () => {
   });
 
   describe('singup', () => {
+    const mockSignUpDto: SignUpDto = {
+      email: 'test@email.com',
+      name: 'test_name',
+      password: 'test_password',
+    };
+
     it('should be defined', () => {
       expect(service.signup).toBeDefined();
+    });
+
+    it('should call create user', async () => {
+      await service.signup(mockSignUpDto);
+
+      expect(mockUsersService.create).toBeCalled();
     });
   });
 });
