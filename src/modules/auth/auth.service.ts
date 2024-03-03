2a964c68985c23ce10555163fa59301a0dc66e35
cslee0034 <cslee0034@gmail.com>
2024-03-03 12:14:13 +0900

    feat: signup에서 create user 호출 비즈니스 로직 추가

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { SignUpDto } from './dto/signup.dto';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  signup(signUpDto: SignUpDto) {
-    return;
+  constructor(private readonly usersService: UsersService) {}
+
+  async signup(signUpDto: SignUpDto) {
+    const createdUser = await this.usersService.create(signUpDto);
+    return createdUser;
   }
 }
