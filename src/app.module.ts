d6f1e25ddfcbf32d573975ce466572634c815ebc
cslee0034 <cslee0034@gmail.com>
2024-03-03 17:13:22 +0900

    fix: jwt module과 passport 모듈 삭제
    library로 따로 정의해둔 모듈 삭제하고 직접 import 해서 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,8 +8,6 @@ import { LoggerModule } from './library/logger/logger.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { EventModule } from './library/message/event.module';
 import { PrismaModule } from './library/orm/prisma.module';
-import { JWTModule } from './library/token/jwt.module';
-import { JWTPassportModule as PassportModule } from './library/token/passport.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { EncryptModule } from './modules/encrypt/encrypt.module';
@@ -22,8 +20,6 @@ import { EncryptModule } from './modules/encrypt/encrypt.module';
     LoggerModule,
     EventModule,
     PrismaModule,
-    JWTModule,
-    PassportModule,
     AuthModule,
     EncryptModule,
     UsersModule,
