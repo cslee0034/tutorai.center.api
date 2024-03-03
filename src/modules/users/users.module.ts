49e073105f352d671cd95659e319a61f4c355aae
cslee0034 <cslee0034@gmail.com>
2024-03-03 13:42:40 +0900

    feat: UsersModule에 Encrypt 모듈 import

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UserRepository } from './users.repository';
+import { EncryptModule } from '../encrypt/encrypt.module';
 
 @Module({
+  imports: [EncryptModule],
   controllers: [UsersController],
   providers: [UsersService, UserRepository],
   exports: [UsersService],
