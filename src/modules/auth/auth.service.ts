c116ce3b1d67889558a04ea3b1a7c4f3a1c3e5d1
cslee0034 <cslee0034@gmail.com>
2024-03-03 17:15:28 +0900

    feat: generate token 비즈니스 로직 작성

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { SignUpDto } from './dto/signup.dto';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async generateToken(userId: number, email: string) {
+    const payload = {
+      sub: userId,
+      email: email,
+    };
+
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get<string>('jwt.access.secret'),
+        expiresIn: this.configService.get<string>('jwt.access.expiresIn'),
+      }),
+      this.jwtService.signAsync(payload, {
+        secret: this.configService.get<string>('jwt.refresh.secret'),
+        expiresIn: this.configService.get<string>('jwt.refresh.expiresIn'),
+      }),
+    ]);
+
+    return { accessToken, refreshToken };
+  }
 
   async signup(signUpDto: SignUpDto) {
     const createdUser = await this.usersService.create(signUpDto);
