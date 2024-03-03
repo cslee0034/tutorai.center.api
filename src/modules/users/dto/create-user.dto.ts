58f15338b46ff782fd6f5114f6adc34b6e223879
cslee0034 <cslee0034@gmail.com>
2024-03-03 13:42:16 +0900

    fix: createUserDto에 ApiProperty 삭제

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,16 +1,12 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty()
   @IsEmail()
   email: string;
 
-  @ApiProperty()
   @MaxLength(30)
   name: string;
 
-  @ApiProperty()
   @MaxLength(30)
   password: string;
 }
