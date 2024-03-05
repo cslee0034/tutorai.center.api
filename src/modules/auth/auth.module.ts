62b749dff5cd59894c74fcc9831e004dd8c2aee4
cslee0034 <cslee0034@gmail.com>
2024-03-06 00:21:42 +0900

    fix: user.module에 대한 forwarRef 삭제
    순환 종속성이 예상되지 않기 때문에 삭제

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,7 +6,7 @@ import { LocalStrategy } from './strategies/local.strategy';
 import { EncryptModule } from '../encrypt/encrypt.module';
 
 @Module({
-  imports: [EncryptModule, forwardRef(() => UsersModule)],
+  imports: [EncryptModule, UsersModule],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
   exports: [AuthService],
