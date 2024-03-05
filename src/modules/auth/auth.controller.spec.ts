56a9feee88ae491d478e92d4be04e5b6b04c6432
cslee0034 <cslee0034@gmail.com>
2024-03-05 23:16:56 +0900

    fix: contoller -> controller 이름 수정

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AuthController } from './auth.contoller';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
 import { SignInDto } from './dto/signin.dto';
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
-import { AuthController } from './auth.contoller';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 import { EncryptModule } from '../encrypt/encrypt.module';
