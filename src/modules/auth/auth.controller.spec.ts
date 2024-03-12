e77e15ce187aa7f35c0df0b7e9e92b3beca5a828
cslee0034 <cslee0034@gmail.com>
2024-03-13 06:44:07 +0900

    fix: auth.controller.test 수정

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -6,72 +6,145 @@ import { EncryptService } from '../encrypt/encrypt.service';
 import { UsersService } from '../users/users.service';
 import { LoginDto } from './dto/request/login.dto';
 import { UserEntity } from '../users/entities/user.entity';
+import { Tokens } from './types/tokens.type';
+import {
+  ForbiddenException,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let authService: any;
+  let usersService: any;
+  let encryptService: any;
 
-  const mockAuthSerivce = {
-    signup: jest.fn((signUpDto: SignUpDto) => {
-      return { accessToken: 'accessToken', refreshToken: 'refreshToken' };
+  const mockSignUpDto: SignUpDto = {
+    email: 'test@email.com',
+    name: 'test_name',
+    password: 'test_password',
+  };
+
+  const mockLoginDto: LoginDto = {
+    email: 'test@email.com',
+    password: 'test_password',
+  };
+
+  const mockCreateUserResult: UserEntity = new UserEntity({
+    id: 1,
+    email: 'test@email.com',
+    name: 'test_name',
+    password: 'hashed_test_password',
+  });
+
+  const mockFindOneByEmailResult: UserEntity = new UserEntity({
+    id: 1,
+    email: 'test@email.com',
+    name: 'test_name',
+    password: 'hashed_test_password',
+  });
+
+  const mockTokenResult: Tokens = {
+    accessToken: 'accessToken',
+    refreshToken: 'refreshToken',
+  };
+
+  const mockAuthService = {
+    login: jest
+      .fn()
+      .mockImplementation(
+        (id: string, refreshToken: string): Promise<boolean> => {
+          if (id && refreshToken) {
+            return Promise.resolve(true);
+          } else {
+            return Promise.reject(
+              new InternalServerErrorException('Failed to set refresh token'),
+            );
+          }
+        },
+      ),
+
+    logout: jest.fn().mockImplementation((id: string): Promise<boolean> => {
+      if (id) {
+        return Promise.resolve(true);
+      } else {
+        return Promise.reject(
+          new InternalServerErrorException('Failed to delete refresh token'),
+        );
+      }
     }),
 
-    signin: jest.fn(),
-
-    login: jest.fn(() => {
-      return true;
-    }),
-
-    logout: jest.fn(),
-
-    generateToken: jest.fn((userId: number, email: string) => {
-      return {
-        accessToken: 'accessToken',
-        refreshToken: 'refreshToken',
-      };
-    }),
+    generateToken: jest
+      .fn()
+      .mockImplementation((userId: number, email: string): Promise<Tokens> => {
+        if (userId && email) {
+          return Promise.resolve(mockTokenResult);
+        } else {
+          return Promise.reject(
+            new InternalServerErrorException('Failed to create token'),
+          );
+        }
+      }),
   };
 
   const mockUsersService = {
-    create: jest.fn(() => {
-      return {
-        id: 1,
-        email: 'example@email.com',
-      };
-    }),
+    create: jest
+      .fn()
+      .mockImplementation((mockSignUpDto: SignUpDto): Promise<UserEntity> => {
+        if (
+          mockSignUpDto.email === 'test@email.com' &&
+          mockSignUpDto.password === 'test_password' &&
+          mockSignUpDto.name === 'test_name'
+        ) {
+          return Promise.resolve(mockCreateUserResult);
+        } else {
+          return Promise.reject(new ForbiddenException('User already exists'));
+        }
+      }),
 
-    findOneByEmail: jest.fn((email: string) => {
+    findOneByEmail: jest.fn((email: string): Promise<UserEntity> => {
       if (email === 'test@email.com') {
-        return new UserEntity({
-          id: 2,
-          email: 'test@email.com',
-          name: 'test_name',
-        });
+        return Promise.resolve(mockFindOneByEmailResult);
+      } else {
+        return Promise.reject(new NotFoundException('User not found'));
       }
     }),
   };
 
   const mockEncryptService = {
-    compare: jest.fn((key: string, hashedKey: string) => {
+    compare: jest.fn((key: string, hashedKey: string): Promise<boolean> => {
       if (hashedKey === 'hashed_' + key) {
-        return true;
+        return Promise.resolve(true);
       }
-      return false;
+      return Promise.resolve(false);
     }),
 
-    compareAndThrow: jest.fn(),
+    compareAndThrow: jest
+      .fn()
+      .mockImplementation((key: string, hashedKey: string): Promise<void> => {
+        if (mockEncryptService.compare(key, hashedKey)) {
+          return Promise.resolve();
+        } else {
+          Promise.reject(new UnauthorizedException('Key does not match'));
+        }
+      }),
   };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [
-        { provide: AuthService, useValue: mockAuthSerivce },
+        { provide: AuthService, useValue: mockAuthService },
         { provide: UsersService, useValue: mockUsersService },
         { provide: EncryptService, useValue: mockEncryptService },
       ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
+    usersService = module.get<UsersService>(UsersService);
+    encryptService = module.get<EncryptService>(EncryptService);
   });
 
   it('should be defined', () => {
@@ -79,69 +152,107 @@ describe('AuthController', () => {
   });
 
   describe('signup', () => {
-    const mockSignUpDto: SignUpDto = {
-      email: 'test@email.com',
-      name: 'test_name',
-      password: 'test_password',
-    };
-
     it('should be defined', () => {
       expect(controller.signup).toBeDefined();
     });
 
     it('should call userService.create with SignUpDto', async () => {
-      await controller.signup(mockSignUpDto);
+      await controller.signup(mockSignUpDto as SignUpDto);
 
-      expect(mockUsersService.create).toBeCalledWith(mockSignUpDto);
+      expect(usersService.create).toBeCalledWith(mockSignUpDto as SignUpDto);
     });
 
     it('should call generateToken with created user information', async () => {
-      await controller.signup(mockSignUpDto);
+      await controller.signup(mockSignUpDto as SignUpDto);
 
-      expect(mockAuthSerivce.generateToken).toBeCalledWith(
-        1,
-        'example@email.com',
+      expect(authService.generateToken).toBeCalledWith(
+        mockCreateUserResult.id as number,
+        mockCreateUserResult.email as string,
       );
     });
 
     it("should call login with user's id and refreshToken", async () => {
-      await controller.signup(mockSignUpDto);
+      await controller.signup(mockSignUpDto as SignUpDto);
 
-      expect(mockAuthSerivce.login).toBeCalledWith(1, 'refreshToken');
+      expect(authService.login).toBeCalledWith(
+        mockCreateUserResult.id as number,
+        mockTokenResult.refreshToken as string,
+      );
+    });
+
+    it('should return tokens', async () => {
+      const result = await controller.signup(mockSignUpDto as SignUpDto);
+
+      expect(result).toEqual(mockTokenResult);
     });
   });
 
   describe('login', () => {
-    const loginDto: LoginDto = {
-      email: 'test@email.com',
-      password: 'test_password',
-    };
     it('should be defined', () => {
       expect(controller.login).toBeDefined();
     });
 
     it('should call userService.findOneByEmail with LoginUpDto', async () => {
-      await controller.login(loginDto);
+      await controller.login(mockLoginDto as LoginDto);
 
-      expect(mockUsersService.findOneByEmail).toBeCalledWith(loginDto.email);
+      expect(usersService.findOneByEmail).toBeCalledWith(
+        mockLoginDto.email as string,
+      );
+    });
+
+    it('should call encryptService.compareAndThrow with password', async () => {
+      await controller.login(mockLoginDto as LoginDto);
+
+      expect(encryptService.compareAndThrow).toBeCalledWith(
+        mockLoginDto.password as string,
+        mockFindOneByEmailResult.password as string,
+      );
     });
 
     it("should call generateToken with found user's information", async () => {
-      await controller.login(loginDto);
+      await controller.login(mockLoginDto as LoginDto);
 
-      expect(mockAuthSerivce.generateToken).toBeCalledWith(2, 'test@email.com');
-    });
-
-    it("should call generateToken with found user's information", async () => {
-      await controller.login(loginDto);
-
-      expect(mockAuthSerivce.generateToken).toBeCalledWith(2, 'test@email.com');
+      expect(authService.generateToken).toBeCalledWith(
+        mockFindOneByEmailResult.id as number,
+        mockFindOneByEmailResult.email as string,
+      );
     });
 
     it("should call login with user's id and refreshToken", async () => {
-      await controller.login(loginDto);
+      await controller.login(mockLoginDto as LoginDto);
 
-      expect(mockAuthSerivce.login).toBeCalledWith(2, 'refreshToken');
+      expect(authService.login).toBeCalledWith(
+        mockFindOneByEmailResult.id as number,
+        mockTokenResult.refreshToken as string,
+      );
+    });
+
+    it('should return tokens', async () => {
+      const result = await controller.login(mockLoginDto as LoginDto);
+
+      expect(result).toEqual(mockTokenResult);
+    });
+  });
+
+  describe('logout', () => {
+    it('should be defined', () => {
+      expect(controller.logout).toBeDefined();
+    });
+
+    it('should call authService.logout with id and refreshToken', async () => {
+      const id = mockCreateUserResult.id as number;
+
+      await controller.logout(id);
+
+      expect(authService.logout).toBeCalledWith(id);
+    });
+
+    it('should return success', async () => {
+      const id = mockCreateUserResult.id as number;
+
+      const result = await controller.logout(id);
+
+      expect(result).toEqual({ success: true });
     });
   });
 });
